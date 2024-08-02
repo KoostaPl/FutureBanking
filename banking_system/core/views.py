0a98bf0656f8fe68eb4769860ced09ccf4e8928b
Kanstantsin <166393112+KoostaPl@users.noreply.github.com>
2024-08-02 10:29:22 +0300

    views.py

--- banking_system/core/views.py
+++ banking_system/core/views.py
@@ -6,10 +6,4 @@ def index(request):
         return redirect("account:account")
     return render(request, "core/index.html")
 
-def contact(request):
-    return render(request, "core/contact.html")
-
-def about(request):
-    return render(request, "core/about.html")
-
 
